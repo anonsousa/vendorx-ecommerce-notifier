e817243f2e8710ab7715463c907cc9f882242901
Antonio <pisouza011@gmail.com>
2024-09-23 21:31:25 -0300

    First Commit :D

---
+++ src/main/java/com/antoniosousa/ecommerce_notifier/EcommerceNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.antoniosousa.ecommerce_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniosousa/ecommerce_notifier/EcommerceNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniosousa.ecommerce_notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
